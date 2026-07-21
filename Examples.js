185061e890e233fe1e980a8cb6d9de437d777916
molenjin <39934180+molenjin@users.noreply.github.com>
2026-07-21 16:59:45 +0800

    Add files via upload

--- Examples.js
+++ Examples.js
@@ -6,33 +6,33 @@ const examplesJson = {
             "seed": "F--F--F",
             "rule": "F+F--F+F",
             "angle": 60,
-            "startAngle": "0",
-            "startX": "0",
-            "startY": "0",
-            "length": "20",
-            "generations": "3"
+            "startAngle": 0,
+            "startX": 0,
+            "startY": 0,
+            "length": 20,
+            "generations": 3
         },
         {
             "name": "Two-branch Tree", 
             "seed": "F",
             "rule": "F[-F][+F]",
             "angle": 10,
-            "startAngle": "-90",
-            "startX": "0",
-            "startY": "0",
-            "length": "40",
-            "generations": "6"
+            "startAngle": -90,
+            "startX": 0,
+            "startY": 0,
+            "length": 40,
+            "generations": 6
         },
         {
             "name": "Three-branch Tree",
             "seed": "F",
             "rule": "F[-F][+F][++F]",
             "angle": 12,
-            "startAngle": "-90",
-            "startX": "0",
-            "startY": "0",
-            "length": "50",
-            "generations": "5"
+            "startAngle": -90,
+            "startX": 0,
+            "startY": 0,
+            "length": 50,
+            "generations": 5
         }                
     ]
 };
